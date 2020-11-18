96ba46cab9572a5dfa9c9e47c1bd5ff5c2d4ad82
Oleg Anohin <oanohin@outlook.com>
2020-11-18 22:03:23 +0400

    Maze Generator v.01

---
+++ Assets/Scripts/Maze/MazeSpawner.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _cellPrefab;
+}
---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void Move(float horizontal, float vertical)
+    {
+        transform.Translate(horizontal * _speed * Time.deltaTime, vertical * _speed * Time.deltaTime, 0);
+    }
+}
