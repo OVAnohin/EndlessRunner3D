4087c3f7b28de575ba32dbda54883a4e22ff3cda
Oleg Anohin <oanohin@outlook.com>
2020-11-21 14:17:17 +0400

    time Commit

---
+++ Assets/Scripts/Camera/CameraMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _waitTime = 1f;
+
+    private void Update()
+    {
+        _waitTime -= Time.deltaTime;
+        if (_waitTime > 0)
+            return;
+
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+}
